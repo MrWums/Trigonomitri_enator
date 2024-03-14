69df0a944e915b0d396ad5a44013d0f354a00a70
MrWums <williamravn2006@gmail.com>
2024-03-14 10:26:27 +0100

    Jeg leger med seje lister

---
+++ Listeting.py
@@ -0,0 +1,17 @@
+sejliste=[0,1,2]
+print(sejliste[0])
+
+for jens in sejliste:
+    print(jens)
+
+print(sejliste[:2])
+
+print(sejliste)
+sejliste.append(4)
+print(sejliste)
+
+sejliste.insert(0,0)
+print(sejliste)
+
+sejliste.remove(0)
+print(sejliste)
